956155f49c2d4b8d577f5f19194e6a467863eb6a
Radojica Popovic <radojicapopovic@icloud.com>
2024-09-07 20:52:02 +0200

    - Header component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AboutMe from "./components/Home/AboutMe";
+import ContactForm from "./components/Home/ContactForm";
+import Experience from "./components/Home/Experience";
+import Footer from "./components/Home/Footer";
+import Header from "./components/Home/Header";
+import Hero from "./components/Home/Hero";
+import Projects from "./components/Home/Projects";
+import Skills from "./components/Home/Skills";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Hero />
+      <AboutMe />
+      <Skills />
+      <Experience />
+      <Projects />
+      <ContactForm />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,5 +1,23 @@
+import Logo from "../../assets/logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between items-center px-[20vw] py-5">
+      <img src={Logo} alt="logo" className="cursor-pointer " />
+      <p className="text-white text-base font-karla">About Me</p>
+      <p className="text-white text-base font-karla ml-[-70px]">Experience</p>
+      <p className="text-white text-base font-karla ml-[-70px]">Projects</p>
+      <p className="text-white text-base font-karla ml-[-70px]">Resume</p>
+      <button
+        className="cursor-pointer text-red hover:before:bg-redborder-[#648e71] relative overflow-hidden  bg-[#4E6E58] px-3 text-white 
+            shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 
+            before:bg-[#648e71] before:transition-all before:duration-500 hover:text-white hover:shadow-[#648e71] hover:before:left-0
+            hover:before:w-full h-[44px] w-[205px]  "
+      >
+        <span className="relative z-10 font-karla ">HIRE ME</span>
+      </button>
+    </div>
+  );
 };
 
 export default Header;
